0c3a70d40b967ddd9934e85b1e7663b853a67eaf
Alperen Aga <alperencode@gmail.com>
2021-10-17 14:37:02 +0300

    Add new files

---
+++ deneme1/Deneme1/Assets/HMDTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HMDTest : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("XRSettings.isDeviceActive");
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
