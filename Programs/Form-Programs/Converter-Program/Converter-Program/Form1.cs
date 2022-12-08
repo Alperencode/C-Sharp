ed24c77ca55b0e2345a8feb8cc6d16dbfdf3d096
Alperen Aga <alperencode@gmail.com>
2022-12-08 20:24:49 +0300

    Add Form-Programs/Converter-Program

---
+++ Programs/Form-Programs/Converter-Program/Converter-Program/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "0";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double result;
+            if (Double.TryParse(textBox1.Text, out result))
+            {
+                double not = (float)Convert.ToDouble(textBox1.Text);
+
+                double resul = (4 * not) / 100;
+
+                label1.Text = resul.ToString();
+            }
+            else
+            {
+                label1.Text = "Invalid input";
+            }
+
+        }
+    }
+}
---
+++ Programs/Form-Programs/Converter-Program/README.md
@@ -0,0 +1,7 @@
+<h3>
+
+- Converter Program
+
+    - Converts a given number in range 0-100 to 0-4 GPA scale
+
+</h3>
